14cb85d091f5c76867b64430c247a995e8fb8502
SharmaTechGroup <147160462+SharmaTechGroup@users.noreply.github.com>
2025-08-29 08:51:12 +0530

    MUI

--- src/video-tutorials/user-dashboard.jsx
+++ src/video-tutorials/user-dashboard.jsx
@@ -1,11 +1,68 @@
-
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie";
+import { Link, useNavigate } from "react-router-dom";
 
 export function UserDashboard(){
+
+    const [videos, setVideos] = useState([{id:0, title:null, description:null, url:null, likes:0, views:0, dislikes:0, comments:null, category_id:0}]);
+   
+    let navigate = useNavigate();
+
+    function handleSignout(){
+        removeCookie('admin_id');
+        navigate('/admin-login');
+    }
+
+    function LoadVideos(){
+        axios.get(`http://localhost:3000/videos`)
+        .then(response=>{
+            setVideos(response.data);
+        })
+    }
+
+    useEffect(()=>{
+         
+            LoadVideos();
+          
+    },[])
     return(
         <div className="bg-light p-2">
-            <h2>User Dashboard</h2>
-
+            <nav className="d-flex justify-content-between">
+                <div>User Dashboard</div>
+                <div>
+                    <div className="input-group">
+                        <input type="text" placeholder="Search Videos" className="form-control" />
+                        <button className="btn btn-warning bi bi-search"></button>
+                    </div>
+                </div>
+                <div>
+                    <span className="bi bi-save"></span>
+                </div>
+            </nav>
+            <div className="d-flex flex-wrap">
+                {
+                    videos.map(video=>
+                        <div style={{width:'250px'}} key={video.id} className="card m-2 p-2">
+                            <iframe className="w-100" src={video.url} height="200" ></iframe>
+                            <div className="card-header" style={{height:'80px'}}>
+                                {video.title}
+                            </div>
+                            <div className="card-body">
+                                {video.description}
+                            </div>
+                            <div className="card-footer text-center">
+                                <span className="bi bi-eye"> {video.views} </span>
+                                <span className="bi bi-hand-thumbs-up mx-3 "> {video.likes} </span>
+                                 <span className="bi bi-hand-thumbs-down "> {video.dislikes} </span>
+                                <div> 
+                                    <button className="btn w-100 btn-success bi bi-save2-fill"></button>
+                                </div> 
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
--- src/video-tutorials/user-login.jsx
+++ src/video-tutorials/user-login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useFormik } from "formik";
 import { Link, useNavigate } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
 
 export function UserLogin(){
 
@@ -29,16 +30,16 @@ export function UserLogin(){
 
 
     return(
-        <div className="bg-light p-2 w-25">
+        <div className="bg-light p-3 w-25">
             <h2>User Login</h2>
             <form onSubmit={formik.handleSubmit}>
-                <dl>
-                    <dt>User Id</dt>
-                    <dd><input onChange={formik.handleChange} type="text" name="user_id"/></dd>
-                    <dt>Password</dt>
-                    <dd><input onChange={formik.handleChange}  type="password" name="password"/></dd>
-                </dl>
-                <button className="btn btn-warning">Login</button>
+                <div className="my-2">
+                    <TextField onChange={formik.handleChange} name="user_id" variant="outlined" label="User Id" fullWidth />
+                </div>
+                 <div className="my-2">
+                    <TextField onChange={formik.handleChange} name="password" type="password" variant="outlined" label="Password" fullWidth />
+                </div>
+                <Button type="submit" variant="contained" color="warning" fullWidth > Login </Button>
             </form>
             <Link to="/user-register" className="btn btn-link">New User Register</Link>
         </div>
